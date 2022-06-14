e7c37da0794e29c0b39fe2b85225b562ae4aff07
Bruno Guimarães <brunoelinton@outlook.com>
2022-06-14 16:26:49 -0300

    Selecting form elements

--- js/controller/form-controller.js
+++ js/controller/form-controller.js
@@ -1,4 +1,28 @@
+
+function State() {
+    this.btnSave = null;
+    this.btnClear = null;
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNunber = null;
+    this.inputCity = null;
+    this.errorCep = null;
+    this.errorNumber = null;
+}
+
+const state = new State();
+
 export function init() {
-    const inputCep = document.forms.newAddres.cep;
-    console.log(inputCep);
+    state.inputCep = document.forms.newAddres.cep;
+    state.inputStreet = document.forms.newAddres.street;
+    state.inputNunber = document.forms.newAddres.number;
+    state.inputCity = document.forms.newAddres.city;
+
+    state.btnSave = document.forms.newAddres.btnSave;
+    state.btnClear = document.forms.newAddres.btnClear;
+    
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
+
+    console.log(state);
 }
