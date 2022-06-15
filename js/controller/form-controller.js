d624a4f6720c08fa9050384d127ba58bdd08085e
Bruno Guimarães <brunoelinton@outlook.com>
2022-06-15 19:33:17 -0300

    Checking address object before saving

--- js/controller/form-controller.js
+++ js/controller/form-controller.js
@@ -61,9 +61,22 @@ function handleInputNumberKeyUp(event) {
     }
  }
 
-async function handleBtnSaveClick(event) {
+ function handleBtnSaveClick(event) {
     event.preventDefault();
-    listController.addCard(state.address);
+
+    const errors = addresService.getErros(state.address);
+    const keys = Object.keys(errors);
+    
+    if(keys.length > 0) {
+        keys.forEach(key => {
+            setFormError(key, errors[key]);
+        });
+    } else {
+        listController.addCard(state.address);
+        clearForm();
+    }
+
+
 }
 
 function handleInputNumberChange(event) {
@@ -87,6 +100,8 @@ function clearForm() {
 
     setFormError("cep", "");
     setFormError("number", "");
+
+    state.address = new Address();
     
     state.inputCep.focus();
 }
--- js/services/address-service.js
+++ js/services/address-service.js
@@ -7,3 +7,17 @@ export async function findByCep(cep) {
     const address = new Address(result.cep, result.logradouro, null, result.localidade);
     return address;
 }
+
+export function getErros(address) {
+    const errors = {};
+
+    if(!address.cep || address.cep == "") {
+        errors.cep = "Campo requirido";
+    }
+
+    if(!address.number || address.number == "") {
+        errors.number = "Campo requirido";
+    }
+
+    return errors;
+}
